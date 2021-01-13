20cf6f199e0247a34193fdf79dd47adaca063d53
vamsivallepu <vallepu670@gmail.com>
2021-01-13 15:25:48 +0530

    updated selection sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -3,7 +3,7 @@ public class SelectionSort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner inp=new Scanner(System.in);
-		System.out.print("Enter number of Elements to be entered in an arry :");
+		System.out.print("Enter number of Elements to be entered in an array :");
 		int n=inp.nextInt();
 		int arr[]=new int[n];
 		System.out.println("Enter the elements of array:");

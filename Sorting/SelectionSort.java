57094829f4780711544bf4dabfa1f0f6ba1ce5be
vamsivallepu <vallepu670@gmail.com>
2021-01-13 15:23:23 +0530

    updated selection sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -25,7 +25,7 @@ public class SelectionSort {
 	private static int[] SelectionSort(int n, int[] arr) {
 
 		int temp;
-		for(int i=0;i<n;i++) {
+		for(int i=0;i<n-1;i++) {
 			int min=i;
 			for(int j=i+1;j<n;j++) {
 				if(arr[j]<arr[min]) {
